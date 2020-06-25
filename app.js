b5781b072349d8ab6c2fd63ac9cab5159ef9e8fd
bradserlis <bradserlis@yahoo.com>
2020-06-25 16:22:21 -0700

    font, buttons, styling added

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 console.log('hello from the other side....')
 
-$('#master').click(()=> console.log('you click me!?'))
+$('#increment-button').click(() => onClick())
+$('#reset-button').click(()=> onReset())
+
+let donationTotalValue = parseInt($('#donation-total').html(), 10)
+
+const onClick = () => {
+  donationTotalValue = (donationTotalValue + 10)
+  $('#donation-total').html(donationTotalValue)
+}
+
+const onReset = () => {
+  donationTotalValue = 0;
+  $('#donation-total').html(donationTotalValue)
+}
